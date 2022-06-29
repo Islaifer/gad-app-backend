14a9c48b020a72c06f7cbc97f63d3b4695f3a4c8
islaifer <islaifer23@gmail.com>
2022-06-28 23:15:04 -0300

    Initial commit

---
+++ src/main/java/com/fatec/gad/GadApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.gad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/gad/GadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.gad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
