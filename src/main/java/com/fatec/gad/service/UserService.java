5bb5eb8fb14132c38ef81d45edf9efd281376197
islaifer <islaifer23@gmail.com>
2022-07-01 11:45:46 -0300

    Implement logger on UserService

--- src/main/java/com/fatec/gad/service/UserService.java
+++ src/main/java/com/fatec/gad/service/UserService.java
@@ -3,6 +3,8 @@ package com.fatec.gad.service;
 import com.fatec.gad.dao.repository.UserRepository;
 import com.fatec.gad.model.entity.User;
 import com.fatec.gad.model.request.UserRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +16,8 @@ public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
@@ -21,25 +25,33 @@ public class UserService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        logger.debug("Start login");
         try{
             return get(login);
         }catch (Exception e){
+            logger.error(e.getMessage());
             throw new UsernameNotFoundException(e.getMessage());
         }
     }
 
     private UserRequest get(String login) throws UsernameNotFoundException{
+        logger.debug("Start catching user");
         User user;
         try{
+            logger.info("Try get user by username");
             user = userRepository.findByUsername(login);
             if(user == null){
+                logger.warn("Username not found. Trying get user by email");
                 user = userRepository.findByEmail(login);
                 if(user == null){
+                    logger.error("User wasn't found!");
                     throw new UsernameNotFoundException("User wasn't found!");
                 }
             }
+            logger.info("User has been found by Username/Password: ".concat(login));
             return UserRequest.create(user);
         }catch (Exception e){
+            logger.error("User wasn't found!");
             throw new UsernameNotFoundException("User wasn't found!");
         }
     }
